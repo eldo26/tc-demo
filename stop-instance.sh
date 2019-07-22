bf8116350e706ae603587cdc59bbaec9d4eb2112
Eldo Joseph <eldo26@gmail.com>
2019-07-22 11:56:41 +0530

    changes

--- stop-instance.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-instance_zone=`gcloud compute instances list |awk '{print $2}' |sed '1d' |head -1`
-for instance_id in $(gcloud compute instances list |awk '{print $1}' |sed '1d')
-do
-	gcloud compute instances stop $instance_id --zone=$instance_zone --quiet
-done
